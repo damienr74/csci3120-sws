878565f57d3e9e5ad5bf043601dacae8ba6db888
Amirreza Abbasnejad <rezdamir@gmail.com>
2017-04-05 20:56:53 -0300

    Add MLQF clean up scheduler.c

--- scheduler.c
+++ scheduler.c
@@ -31,7 +31,7 @@
 #define MAX_HTTP_SIZE 8192 /* size of buffer to allocate */
 
 static void *new(const void *_interface, ...);
-static void delete( void *this );
+static void delete(void *this);
 void *scheduler_run(void *arg);
 
 static struct scheduler *sched = NULL;
@@ -40,20 +40,20 @@ static pthread_mutex_t request_mutex;
 static pthread_cond_t cond;
 static char *buffer;
 
-void scheduler_init( char *name, int thrd_count ) {
-	if ( !name || thrd_count < 1 )
+void scheduler_init(char *name, int thrd_count) {
+	if (!name || thrd_count < 1)
 		return;
 
-	if ( !strcmp( "SJF", name ) ) {
+	if (!strcmp("SJF", name)) {
 		// init Shortest Job First
-		if ( !(sched = new(Sjf_scheduler)) ) {
-			perror( "Could not init SJF scheduler" );
+		if (!(sched = new(Sjf_scheduler))) {
+			perror("Could not init SJF scheduler");
 			abort();
 		}
-	} else if ( !strcmp( "RR", name ) ) {
+	} else if (!strcmp("RR", name)) {
 		// init Round Robin
-		if ( !(sched = new(Rr_scheduler)) ) {
-			perror( "Could not init SJF scheduler" );
+		if (!(sched = new(Rr_scheduler))) {
+			perror("Could not init SJF scheduler");
 			abort();
 		}
 	} else if (!strcmp("MLQF", name)) {
@@ -72,12 +72,12 @@ void scheduler_init( char *name, int thrd_count ) {
 	pthread_cond_init(&cond, NULL);
 	pthread_t scheduler;
 	if (pthread_create(&scheduler, NULL, &scheduler_run, NULL)) {
-		perror( "Could not start scheduler thread" );
+		perror("Could not start scheduler thread");
 		abort();
 	}
 }
 
-void scheduler_insert( int fd ) {
+void scheduler_insert(int fd) {
 	static char *buffer;
 	char *req = NULL;
 	char *brk;
@@ -85,19 +85,19 @@ void scheduler_insert( int fd ) {
 	int len;
 	struct stat buf;
 
-	if ( fd < 0)
+	if (fd < 0)
 		return;
 
-	if ( !buffer ) {
+	if (!buffer) {
 		buffer = calloc(MAX_HTTP_SIZE, sizeof *buffer);
-		if ( !buffer ) {
-			perror( "Error while allocating memory" );
+		if (!buffer) {
+			perror("Error while allocating memory");
 			abort();
 		}
 	}
 
-	if ( read( fd, buffer, MAX_HTTP_SIZE ) <= 0 ) {
-		perror( "Error while reading request" );
+	if (read(fd, buffer, MAX_HTTP_SIZE) <= 0) {
+		perror("Error while reading request");
 		abort();
 	}
 
@@ -109,8 +109,8 @@ void scheduler_insert( int fd ) {
 		return;
 	}
 
-	if ( (stat(++req, &buf)) ) {
-		len = sprintf( buffer, "HTTP/1.1 404 File not found\n\n");
+	if ((stat(++req, &buf))) {
+		len = sprintf(buffer, "HTTP/1.1 404 File not found\n\n");
 		write(fd, buffer, len);
 		close(fd);
 		return;
@@ -120,7 +120,6 @@ void scheduler_insert( int fd ) {
 	if (!request)
 		return;
 
-
 	pthread_mutex_lock(&request_mutex);
 
 	sched->insert(request);
@@ -129,7 +128,7 @@ void scheduler_insert( int fd ) {
 	pthread_mutex_unlock(&request_mutex);
 }
 
-struct rcb *scheduler_next( void ) {
+struct rcb *scheduler_next(void) {
 	pthread_mutex_lock(&request_mutex);
 	while (sched->rcb_count < 1)
 		pthread_cond_wait(&cond, &request_mutex);
@@ -145,7 +144,7 @@ void *scheduler_run(void *arg) {
 	if (!buffer) {
 		buffer = calloc(MAX_HTTP_SIZE, sizeof *buffer);
 		if (!buffer) {
-			perror( "Error allocating memory" );
+			perror("Error allocating memory");
 			abort();
 		}
 	}
@@ -153,11 +152,11 @@ void *scheduler_run(void *arg) {
 	for (request = scheduler_next();; request = scheduler_next()) {
 		if (request) {
 			if (!request->file) {
-				len = sprintf( buffer, "HTTP/1.1 404 File not found\n\n" );
-				write( request->fd, buffer, len );
+				len = sprintf(buffer, "HTTP/1.1 404 File not found\n\n");
+				write(request->fd, buffer, len);
 			} else {
 				len = sprintf(buffer, "HTTP/1.1 200 OK\n\n");
-				write( request->fd, buffer, len );
+				write(request->fd, buffer, len);
 				// set rcb to started
 			}
 
@@ -168,14 +167,13 @@ void *scheduler_run(void *arg) {
 	return arg;
 }
 
-static void scheduler_serve(struct rcb *request)
-{
+static void scheduler_serve(struct rcb *request) {
 	if (!request)
 		return;
 
 	int len = fread(buffer, 1, MAX_HTTP_SIZE, request->file);
-	if ( len < 0 ) {
-		perror( "Error while reading file" );
+	if (len < 0) {
+		perror("Error while reading file");
 		fflush(stderr);
 		delete(request);
 		return;
@@ -183,59 +181,58 @@ static void scheduler_serve(struct rcb *request)
 
 	request->snt_bytes += len;
 	if (len > 0)
-		len = write( request->fd, buffer, len );
+		len = write(request->fd, buffer, len);
 
-	if ( len < 0 ) {
-		perror( "Error while writing to client" );
+	if (len < 0) {
+		perror("Error while writing to client");
 		fflush(stderr);
 		delete(request);
 		return;
 	}
 }
 
-static void *new( const void *this, ... ) {
+static void *new(const void *this, ...) {
 	const struct interface *interface = this;
-	void *instance = calloc( 1, interface->size );
+	void *instance = calloc(1, interface->size);
 
 	if (!instance)
 		return instance;
 
-	*( const struct interface **)instance = interface;
+	*(const struct interface **) instance = interface;
 
-	if ( interface->new ) {
+	if (interface->new) {
 		va_list args;
-		va_start( args, this );
-		instance = interface->new( instance, &args );
-		va_end( args );
+		va_start(args, this);
+		instance = interface->new(instance, &args);
+		va_end(args);
 	}
 
 	return instance;
 }
 
-static void delete( void *this ) {
+static void delete(void *this) {
 	const struct interface **interface = this;
 
-	if ( this && *interface && (*interface)->delete )
+	if (this && *interface && (*interface)->delete)
 		this = (*interface)->delete(this);
 
 	free(this);
 	this = NULL;
 }
 
-
 /*****************************************************************************
  *			Request Control Block static methods & Implementation
  ****************************************************************************/
 
-static void *rcb_new( void *_this, va_list *args ) {
+static void *rcb_new(void *_this, va_list *args) {
 	struct rcb *this = _this;
 
-	this->seq_num = va_arg( *args, long long );
-	this->fd = va_arg( *args, int );
-	char *filename = va_arg( *args, char * );
-	this->tot_bytes = va_arg( *args, off_t );
+	this->seq_num = va_arg(*args, long long);
+	this->fd = va_arg(*args, int);
+	char *filename = va_arg(*args, char *);
+	this->tot_bytes = va_arg(*args, off_t);
 
-	this->request = malloc( strlen(filename) + 1 );
+	this->request = malloc(strlen(filename) + 1);
 	sprintf(this->request, "%s", filename);
 	this->file = fopen(this->request, "rb");  //**** rb?
 
@@ -246,7 +243,7 @@ static void *rcb_new( void *_this, va_list *args ) {
 	return this;
 }
 
-static void *rcb_delete( void *_this ) {
+static void *rcb_delete(void *_this) {
 	struct rcb *this = _this;
 	free(this->request);
 	fclose(this->file);
@@ -254,35 +251,29 @@ static void *rcb_delete( void *_this ) {
 	return this;
 }
 
-static const struct interface _rcb = {
-	sizeof( struct rcb ),
-	rcb_new,
-	rcb_delete,
-};
+static const struct interface _rcb =
+		{ sizeof(struct rcb), rcb_new, rcb_delete, };
 
 const void *Rcb = &_rcb;
-
 
 /*****************************************************************************
  *			SJF Scheduler static methods & Implementation
  ****************************************************************************/
 
-
-static int sjf_compare( const struct rcb *rcb1, const struct rcb *rcb2 ) {
-	if ( rcb1->tot_bytes < rcb2->tot_bytes )
+static int sjf_compare(const struct rcb *rcb1, const struct rcb *rcb2) {
+	if (rcb1->tot_bytes < rcb2->tot_bytes)
 		return -1;
-	else if ( rcb1->tot_bytes == rcb2->tot_bytes )
+	else if (rcb1->tot_bytes == rcb2->tot_bytes)
 		return 0;
 
 	return 1;
 }
 
 static void sjf_insert(struct rcb *request) {
-	if ( sched->rcb_count >= sched->capacity ) {
-		void *p = realloc(sched->rcbs,
-				sizeof(void *) * sched->capacity * 2);
+	if (sched->rcb_count >= sched->capacity) {
+		void *p = realloc(sched->rcbs, sizeof(void *) * sched->capacity * 2);
 		if (!p) {
-			perror( "Too many requests, out of memory" );
+			perror("Too many requests, out of memory");
 			return;
 		}
 
@@ -293,7 +284,7 @@ static void sjf_insert(struct rcb *request) {
 	int parent;
 	int index = sched->rcb_count++;
 
-	while ( index > 0 ) {
+	while (index > 0) {
 		parent = (index - 1) >> 1;
 		if (sched->compare(sched->rcbs[parent], request) < 0) {
 			break;
@@ -306,7 +297,7 @@ static void sjf_insert(struct rcb *request) {
 	sched->rcbs[index] = request;
 }
 
-static struct rcb *sjf_remove( void ) {
+static struct rcb *sjf_remove(void) {
 	struct rcb *request;
 	int index, next_index, lchild, rchild;
 
@@ -320,17 +311,16 @@ static struct rcb *sjf_remove( void ) {
 		lchild = (index << 1) + 1;
 		rchild = (index << 1) + 2;
 
-		if (lchild < sched->rcb_count &&
-				cmp(sched->rcbs[lchild], new_top) < 0) {
-			if (rchild < sched->rcb_count &&
-				cmp(sched->rcbs[lchild],
-					sched->rcbs[rchild]) < 0) {
+		if (lchild < sched->rcb_count
+				&& cmp(sched->rcbs[lchild], new_top) < 0) {
+			if (rchild < sched->rcb_count
+					&& cmp(sched->rcbs[lchild], sched->rcbs[rchild]) < 0) {
 				next_index = lchild;
 			} else {
 				next_index = rchild;
 			}
-		} else if (rchild < sched->rcb_count &&
-				cmp(sched->rcbs[rchild], new_top) < 0) {
+		} else if (rchild < sched->rcb_count
+				&& cmp(sched->rcbs[rchild], new_top) < 0) {
 			next_index = rchild;
 		} else {
 			sched->rcbs[index] = new_top;
@@ -344,7 +334,7 @@ static struct rcb *sjf_remove( void ) {
 	return request;
 }
 
-static void sjf_serve( struct rcb *request ) {
+static void sjf_serve(struct rcb *request) {
 	do {
 		scheduler_serve(request);
 	} while (request && request->snt_bytes < request->tot_bytes);
@@ -356,7 +346,7 @@ static void sjf_serve( struct rcb *request ) {
 	}
 }
 
-static void *sjf_new( void *_this, va_list *args ) {
+static void *sjf_new(void *_this, va_list *args) {
 	struct scheduler *this = _this;
 
 	this->compare = sjf_compare;
@@ -364,7 +354,7 @@ static void *sjf_new( void *_this, va_list *args ) {
 	this->remove = sjf_remove;
 	this->serve = sjf_serve;
 
-	this->rcbs = calloc( NUM_RCBS, sizeof( void * ) );
+	this->rcbs = calloc(NUM_RCBS, sizeof(void *));
 
 	for (int i = 0; i < NUM_RCBS; i++)
 		this->rcbs[i] = NULL;
@@ -376,7 +366,7 @@ static void *sjf_new( void *_this, va_list *args ) {
 	return this;
 }
 
-static void *sjf_delete( void *_this ) {
+static void *sjf_delete(void *_this) {
 	struct scheduler *this = _this;
 
 	for (int i = 0; i < this->rcb_count; i++)
@@ -387,57 +377,48 @@ static void *sjf_delete( void *_this ) {
 	return this;
 }
 
-static const struct interface _sjf_scheduler = {
-	sizeof( struct scheduler ),
-	sjf_new,
-	sjf_delete,
-};
+static const struct interface _sjf_scheduler = { sizeof(struct scheduler),
+		sjf_new, sjf_delete, };
 
 const void *Sjf_scheduler = &_sjf_scheduler;
-
 
 /*****************************************************************************
  *           RR Scheduler static methods & Implementation
  *****************************************************************************/
 
-static void linkedlist_insert(struct rcb *request, struct rcb *ll[])
-{
+static void linkedlist_insert(struct rcb *request, struct rcb *ll[]) {
 	if (!ll[1]) {
-			ll[0] = ll[1] = request;
-		} else {
-			ll[1]->next = request;
-			ll[1] = ll[1]->next;
-		}
+		ll[0] = ll[1] = request;
+	} else {
+		ll[1]->next = request;
+		ll[1] = ll[1]->next;
+	}
 
-		sched->rcb_count++;
+	sched->rcb_count++;
 }
 
-static struct rcb* linkedlist_remove(struct rcb* list[])
-{
+static struct rcb* linkedlist_remove(struct rcb* list[]) {
 	struct rcb *request = list[0];
-		if (request == list[1]) {
-			list[0] = list[1] = NULL;
-		} else {
-			list[0] = request->next;
-		}
-		sched->rcb_count--;
-		request->next = NULL;
+	if (request == list[1]) {
+		list[0] = list[1] = NULL;
+	} else {
+		list[0] = request->next;
+	}
+	sched->rcb_count--;
+	request->next = NULL;
 
-		return request;
+	return request;
 }
 
-static void rr_insert(struct rcb *request)
-{
+static void rr_insert(struct rcb *request) {
 	linkedlist_insert(request, &sched->rcbs[0]);
 }
 
-static struct rcb *rr_remove(void)
-{
+static struct rcb *rr_remove(void) {
 	return linkedlist_remove(&sched->rcbs[0]);
 }
 
-static void rr_serve(struct rcb *request)
-{
+static void rr_serve(struct rcb *request) {
 	scheduler_serve(request);
 
 	if (request) {
@@ -451,8 +432,7 @@ static void rr_serve(struct rcb *request)
 	}
 }
 
-void *rr_new(void *_this, va_list *args)
-{
+void *rr_new(void *_this, va_list *args) {
 	struct scheduler *this = _this;
 
 	this->insert = rr_insert;
@@ -465,8 +445,7 @@ void *rr_new(void *_this, va_list *args)
 	return this;
 }
 
-void *rr_delete(void *_this)
-{
+void *rr_delete(void *_this) {
 	struct scheduler *this = _this;
 
 	if (this->rcbs[0]) {
@@ -483,50 +462,37 @@ void *rr_delete(void *_this)
 	return this;
 }
 
-
-static const struct interface _Rr_scheduler = {
-	sizeof (struct scheduler),
-	rr_new,
-	rr_delete,
-};
+static const struct interface _Rr_scheduler = { sizeof(struct scheduler),
+		rr_new, rr_delete, };
 
 const void *Rr_scheduler = &_Rr_scheduler;
 
 /*****************************************************************************
  *				MLQF Scheduler static methods & Implementation
  ****************************************************************************/
-static void mlqf_insert(struct rcb *request)
-{
-// 8kb
-	if ( request->status == RCB_8K){
+static void mlqf_insert(struct rcb *request) {
+	if (request->status == RCB_8K) {
 		linkedlist_insert(request, &sched->rcbs[0]);
-	}
-	else if(request->status == RCB_64K ){
+	} else if (request->status == RCB_64K) {
 		linkedlist_insert(request, &sched->rcbs[2]);
 
-	}
-	else if(request->status == RCB_RR ) {
+	} else if (request->status == RCB_RR) {
 		linkedlist_insert(request, &sched->rcbs[4]);
 	}
 }
 
-static struct rcb *mlqf_remove(void)
-{
-// check 8k
-	if (sched->rcbs[0]){
+static struct rcb *mlqf_remove(void) {
+	if (sched->rcbs[0]) {
 		return linkedlist_remove(&sched->rcbs[0]);
-	}
-	else if (sched->rcbs[2]){
+	} else if (sched->rcbs[2]) {
 		return linkedlist_remove(&sched->rcbs[2]);
-	}
-	else if (sched->rcbs[4]){
+	} else if (sched->rcbs[4]) {
 		return linkedlist_remove(&sched->rcbs[4]);
 	}
 	return NULL;
 }
 
-static void mlqf_serve(struct rcb *request)
-{
+static void mlqf_serve(struct rcb *request) {
 	if (request->status == RCB_8K) {
 
 		scheduler_serve(request);
@@ -537,8 +503,7 @@ static void mlqf_serve(struct rcb *request)
 		if (request->snt_bytes < request->tot_bytes) {
 			request->status++;
 			sched->insert(request);
-		}
-		else {
+		} else {
 			delete(request);
 			fflush(stdout);
 		}
@@ -556,8 +521,7 @@ static void mlqf_serve(struct rcb *request)
 		if (request->snt_bytes < request->tot_bytes) {
 			request->status++;
 			sched->insert(request);
-		}
-		else {
+		} else {
 			delete(request);
 			fflush(stdout);
 		}
@@ -573,16 +537,14 @@ static void mlqf_serve(struct rcb *request)
 
 		if (request->snt_bytes < request->tot_bytes) {
 			sched->insert(request);
-		}
-		else {
+		} else {
 			delete(request);
 			fflush(stdout);
 		}
 	}
 }
 
-void *mlqf_new(void *_this, va_list *args)
-{
+void *mlqf_new(void *_this, va_list *args) {
 	struct scheduler *this = _this;
 
 	this->insert = mlqf_insert;
@@ -595,8 +557,7 @@ void *mlqf_new(void *_this, va_list *args)
 	return this;
 }
 
-void *mlqf_delete(void *_this)
-{
+void *mlqf_delete(void *_this) {
 	struct scheduler *this = _this;
 
 	if (this->rcbs[0]) {
@@ -613,10 +574,7 @@ void *mlqf_delete(void *_this)
 	return this;
 }
 
-static const struct interface _Mlqf_scheduler = {
-	sizeof (struct scheduler),
-	mlqf_new,
-	mlqf_delete,
-};
+static const struct interface _Mlqf_scheduler = { sizeof(struct scheduler),
+		mlqf_new, mlqf_delete, };
 
 const void *Mlqf_scheduler = &_Mlqf_scheduler;
